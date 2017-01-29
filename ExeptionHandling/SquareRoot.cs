8a55059729fa4c92ea750e11ac529ab1ae298ae7
BoryanaZhelezova <boryana.zhelezova@gmail.com>
2017-01-29 22:36:19 +0200

    add

---
+++ ExeptionHandling/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            try
+            {
+                double number = double.Parse(input);
+                if (number >= 0)
+                {
+                    double result = Math.Sqrt(number);
+                    Console.WriteLine("{0:F3}", result);
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("number", "Number must be positive!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
